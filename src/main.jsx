4652ba8e0f71fa04be33e639768526ddb12b0f96
afanurkholik <137622326+afanurkholik@users.noreply.github.com>
2026-09-13 21:15:20 +0700

    Update translate main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AppProvider } from './context/AppContext.jsx' // <-- TAMBAHKAN INI
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppProvider> {/* <-- WRAP APP DENGAN PROVIDER */}
+      <App />
+    </AppProvider>
   </React.StrictMode>,
 )
